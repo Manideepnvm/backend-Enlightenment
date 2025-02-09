01b6ae5aca49f43d70b813260fa8e03d458de32b
Manideepnvm <150888483+Manideepnvm@users.noreply.github.com>
2025-02-10 00:42:52 +0530

    first commit

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ import express from 'express'; // ✅ Use import instead of require
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';   
+import subscriptionRouter from './routes/subscription.routes.js';   
+
 import { PORT } from './config/env.js' ; // ✅ Import the PORT variable from the env.js file
 
 const app = express();
+
+app.use('.api/v1/auth', authRouter);
+app.use('.api/v1/user', userRouter);
+app.use('.api/v1/subscriptions', subscriptionRouter);
 
 app.use(morgan('dev'));
 app.use(express.json());
